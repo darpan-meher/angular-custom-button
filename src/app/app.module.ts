022b2bcb7cfb47d10b572caa87b512fa89fac148
Darpa Meher <darpan.meher@gmail.com>
2025-11-19 00:43:30 +0530

    folder restructuring
    folder restructuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CustomButtonComponent } from './custom-button/custom-button.component';
 import { ButtonOverviewComponent } from './button-overview/button-overview.component';
-import { OutlineButtonComponent } from './outline-button/outline-button.component';
-import { SizeButtonComponent } from './size-button/size-button.component';
-import { DisabledButtonComponent } from './disabled-button/disabled-button.component';
-import { VariantButtonComponent } from './variant-button/variant-button.component';
-import { CustomStyleButtonComponent } from './custom-style-button/custom-style-button.component';
+import { OutlineButtonComponent } from './button-overview/components/outline-button/outline-button.component';
+import { SizeButtonComponent } from './button-overview/components/size-button/size-button.component';
+import { DisabledButtonComponent } from './button-overview/components/disabled-button/disabled-button.component';
+import { VariantButtonComponent } from './button-overview/components/variant-button/variant-button.component';
+import { CustomStyleButtonComponent } from './button-overview/components/custom-style-button/custom-style-button.component';
 
 @NgModule({
   declarations: [AppComponent, CustomButtonComponent, ButtonOverviewComponent, OutlineButtonComponent, SizeButtonComponent, DisabledButtonComponent, VariantButtonComponent, CustomStyleButtonComponent],
